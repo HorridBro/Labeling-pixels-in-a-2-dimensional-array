596490db24f9624f13fdd4adef2460f9778d51a0
HorridBro <mihneasaracin@yahoo.com>
2019-01-30 23:22:07 +0200

    Modified arg parsing

--- cmake-build-debug/CMakeFiles/output.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/output.dir/DependInfo.cmake
@@ -4,14 +4,13 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/mihnea/CLionProjects/Labeling-pixels-in-a-2-dimensional-array/main.cpp" "/home/mihnea/CLionProjects/Labeling-pixels-in-a-2-dimensional-array/cmake-build-debug/CMakeFiles/output.dir/main.cpp.o"
+  "/home/horrid/CLionProjects/ParallelConcurrentProgramming/Labeling-pixels-in-a-2-dimensional-array/main.cpp" "/home/horrid/CLionProjects/ParallelConcurrentProgramming/Labeling-pixels-in-a-2-dimensional-array/cmake-build-debug/CMakeFiles/output.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/local/include"
-  "/usr/local/include/opencv"
+  "/usr/local/include/opencv4"
   )
 
 # Targets to which this target links.
--- main.cpp
+++ main.cpp
@@ -271,28 +271,33 @@ Mat create_output_mat(uchar* data, int rows, int cols){
 
 
 int main(int argc, char* argv[]) {
-    Mat img = imread(argv[1]);
     int show_images = 0,  method = 0;
+    string usage = "Usage:\t" + string(argv[0]) +" image_path [-hsv] [-show_images]\n";
     if (argc < 2){
-        cout << "Error, invalid args! " << "Usage:\t" << argv[0] << " image_path [-hsv] [-show_images]\n";
-        exit(1);
+        cout << "Error, invalid args! " << usage;
+        return 1;
     }
-    for(int i = 2; i < argc; ++i){
-        string ar= argv[i];
+    for(int i = 1; i < argc; ++i){
+        string ar = argv[i];
         if (ar == "-hsv"){
             method = 1;
         }
         else if( ar == "-show_images"){
             show_images = 1;
         }
+        else if(ar == "-h"){
+            cout << usage;
+            return 0;
+        }
     }
+    Mat img = imread(argv[1]);
     Mat out = transform_image_2_binary(img, method);
     int rows = out.rows , cols = out.cols;
     int** data = convert_mat(out);
     high_resolution_clock::time_point start = high_resolution_clock::now();
     int** labeled = connected_component_labeling_serial(data, rows, cols);
     high_resolution_clock::time_point stop = high_resolution_clock::now();
-    auto duration = duration_cast<seconds>( stop - start ).count();
+    float duration = duration_cast<chrono::duration<float>>( stop - start ).count();
     cout << "Connected Component Labeling Duration = " << duration << " seconds\n";
     uchar* colored = color_labels(labeled, rows, cols);
     Mat final = create_output_mat(colored, rows, cols);
